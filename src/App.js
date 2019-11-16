224015f19736bb6231611f430a2da6e241115e32
Samuelson Okoi <samuelsonokoi@gmail.com>
2019-11-16 16:59:02 +0100

    added fonts and colors for app with search-box component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
 import { CardsList } from "./components/cards-list/cards-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ""
     };
   }
 
@@ -17,10 +19,23 @@ class App extends Component {
       .then(users => this.setState({ monsters: users }));
   }
 
+  handleChange = e => {
+    this.setState({ searchField: e.target.value });
+  };
+
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
       <div className="App">
-        <CardsList monsters={this.state.monsters} />
+        <h1>Monsters Rolodex App</h1>
+        <SearchBox
+          placeholder="Search monsters here..."
+          handleChange={this.handleChange}
+        />
+        <CardsList monsters={filteredMonsters} />
       </div>
     );
   }
