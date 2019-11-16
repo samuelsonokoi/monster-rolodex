396332f6a39654663450d0ed6d1b2bb3cc65dc29
Samuelson Okoi <samuelsonokoi@gmail.com>
2019-11-16 17:17:27 +0100

    Updates

---
+++ precache-manifest.5bc0e13021b8723c2508b7fda01dec88.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "248b8d55553e120c0af9fc1049da4294",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d03d543ea0537f165e77",
+    "url": "/monsters-rolodex/static/css/main.1d93761b.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/monsters-rolodex/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "d03d543ea0537f165e77",
+    "url": "/monsters-rolodex/static/js/main.c5c96262.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
